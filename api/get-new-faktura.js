8f67b584a860e28be7f223a083094f9a713eb0ad
Geir Gåsodden <geir.gasodden@pythonia.no>
2019-04-28 07:40:01 +0200

    More serverless goodness

---
+++ api/get-new-faktura.js
@@ -0,0 +1,22 @@
+const { send } = require('micro')
+const getUserName = require('../lib/get-user-name')
+const getFaktura = require('../lib/get-faktura')
+const logger = require('../lib/logger')
+const whitelist = ['/docs', '/favicon.ico']
+const machinelist = ['/samtykker']
+
+async function getNewFaktura (request, response) {
+  const { upn } = request.token
+  const userId = getUserName(upn)
+  logger('info', ['getNewFaktura', 'userId', userId])
+  try {
+    const documents = await getFaktura({ batchId: { '$exists': false } })
+    logger('info', ['getNewFaktura', 'userId', userId, 'faktura', documents.length, 'success'])
+    send(response, 200, documents.length)
+  } catch (error) {
+    logger('error', ['getNewFaktura', 'userId', userId, error])
+    send(response, 500, error)
+  }
+}
+
+module.exports = require('../lib/check-token')(whitelist, machinelist, getNewFaktura)
